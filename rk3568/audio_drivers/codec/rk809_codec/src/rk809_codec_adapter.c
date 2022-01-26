74458d6c87524f97bddf155134299a0264227eb2
Jack Huang <huangji@nj.iscas.ac.cn>
2022-01-26 04:47:07 +0000

    append new copyright rules in OAT.xml
    Signed-off-by: Jack Huang <huangji@nj.iscas.ac.cn>

--- rk3568/audio_drivers/codec/rk809_codec/src/rk809_codec_adapter.c
+++ rk3568/audio_drivers/codec/rk809_codec/src/rk809_codec_adapter.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) [2022] [huangji@nj.iscas.ac.cn]
+ * Copyright (c) 2022 huangji@nj.iscas.ac.cn
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
